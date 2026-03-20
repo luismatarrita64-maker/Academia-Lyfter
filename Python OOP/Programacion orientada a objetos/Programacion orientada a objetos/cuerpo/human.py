7f22fd549d177c922c4a87be0b3596ecbe9c45c2
Felipe Rodriguez <luismatarrita64@gmail.com>
2026-03-20 13:26:37 -0600

    Agrego ejercicios de Python OOP

---
+++ Python OOP/Programacion orientada a objetos/Programacion orientada a objetos/cuerpo/human.py
@@ -0,0 +1,20 @@
+class human:
+
+    def __init__(self):
+
+        self.head = head
+        self.torso = torso
+
+        self.left_arm = arm
+        self.right_arm = arm
+
+        self.right_hand = hand
+        self.left_hand = hand
+
+        self.left_leg = leg  
+        self.right_leg = leg 
+
+        self.left_feet = feed
+        self.right_feet = feet
+
+        #
