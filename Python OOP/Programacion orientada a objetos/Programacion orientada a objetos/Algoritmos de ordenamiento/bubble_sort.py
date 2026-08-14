053c4fb4d877621eb5708814b140274c72facb30
Felipe Rodriguez <luismatarrita64@gmail.com>
2026-08-14 13:36:01 -0600

    agrego el ejercicio 1

--- Python OOP/Programacion orientada a objetos/Programacion orientada a objetos/Algoritmos de ordenamiento/bubble_sort.py
+++ Python OOP/Programacion orientada a objetos/Programacion orientada a objetos/Algoritmos de ordenamiento/bubble_sort.py
@@ -1,3 +1,11 @@
+def bubble_sort_original(list):
+    n = len(list)
+    for i in range(n):
+        for j in range(n - 1, i, -1):   
+            if list[j] > list[j - 1]:   
+                list[j], list[j - 1] = list[j - 1], list[j]
+    return list
+
 def bubble_sort(list):
     n = len(list)
     for i in range(n):
@@ -5,6 +13,11 @@ def bubble_sort(list):
             if list[j] < list[j - 1]:   
                 list[j], list[j - 1] = list[j - 1], list[j]
     return list
+
+numbers = [5, 2, 8, 1, 9, 3]
+
+result = bubble_sort_original(numbers)
+print(result)
 
 numbers = [5, 2, 8, 1, 9, 3]
 
